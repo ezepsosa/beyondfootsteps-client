bf986ebede85e398b5a7506a5738dbfaf983a521
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-07 17:47:31 +0200

    feat: integrate react-query for improved data fetching in main application

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,15 +2,19 @@ import { createRoot } from "react-dom/client";
 import "../styles/styles.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { AppRoutes } from "@/routes/AppRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new ApolloClient({
   uri: "https://beyondfootsteps-api.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <ApolloProvider client={client}>
       <AppRoutes />
     </ApolloProvider>
+  </QueryClientProvider>
 );
