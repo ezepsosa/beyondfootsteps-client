43b8b2be65367c0fdc95119dd0a46e9be99a9c27
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-08-07 23:57:17 +0200

    feat: add MetricLayer component with styled KpiSpan and type definitions

---
+++ src/components/map/layer/metric/index.tsx
@@ -0,0 +1,60 @@
+import { LayerGroup, Marker } from "react-leaflet";
+import type { Props } from "./types";
+import { isNumber } from "chart.js/helpers";
+import L from "leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
+import { KpiSpan } from "./style";
+import { humanize } from "@/pages/auxliar";
+
+export const MetricLayer = ({
+  centroids,
+  arrayData,
+  originOrAsylum,
+  metricSelected,
+  setToggleInfo,
+  setToggleCountry,
+}: Props) => {
+  return (
+    <LayerGroup>
+      {arrayData.map((entry) => {
+        let iso: string | undefined;
+        if ("countryOfOriginIso" in entry && originOrAsylum) {
+          iso =
+            originOrAsylum === "asylum"
+              ? entry.countryOfOriginIso ?? undefined
+              : entry.countryOfAsylumIso ?? undefined;
+        } else if ("countryIso" in entry) {
+          iso = entry.countryIso ?? undefined;
+        }
+        if (!iso) return null;
+        const val = (entry as Record<string, unknown>)[metricSelected];
+        const center = centroids[iso];
+        if (!center || !isNumber(val)) return null;
+        return (
+          <Marker
+            key={iso}
+            position={[center[1], center[0]]}
+            icon={L.divIcon({
+              className: "",
+              html: renderToStaticMarkup(
+                <KpiSpan
+                  $fontSize=".7rem"
+                  $fontSizeMD=".8rem"
+                  $fontWeight="600"
+                >
+                  {humanize(val)}
+                </KpiSpan>
+              ),
+            })}
+            eventHandlers={{
+              click: () => {
+                setToggleCountry?.(iso);
+                setToggleInfo?.();
+              },
+            }}
+          />
+        );
+      })}
+    </LayerGroup>
+  );
+};
---
+++ src/components/map/layer/metric/style.ts
@@ -0,0 +1,12 @@
+import { TextSpan } from "@/styles/styles";
+import styled from "styled-components";
+
+export const KpiSpan = styled(TextSpan)`
+  display: flex;
+  z-index: 1001;
+  width: max-content;
+  padding: 2vh;
+  margin-left: -2vh;
+  margin-top: -2vh;
+  position: relative;
+`;
