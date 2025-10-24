821469d76becd252a841504ab4e50e2d0d5d7fbe
Ezequiel Pérez <ezequiel.psosa@outlook.com>
2025-10-24 10:57:58 +0200

    feat: update Apollo Client URI to point to the new GraphQL endpoint

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,7 +5,7 @@ import { AppRoutes } from "@/routes/AppRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new ApolloClient({
-  uri: "https://beyondfootsteps-api.onrender.com/graphql",
+  uri: "https://beyondfootsteps-api-dark-butterfly-7484.fly.dev/graphql",
   cache: new InMemoryCache(),
 });
 
